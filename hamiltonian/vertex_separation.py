618637b60ef0c1dda2168f212efe6ea829e60a44
Victor S. Miller <victor.miller@sri.com>
2025-01-12 10:18:31 -0800

    Debug print for stratified

--- hamiltonian/vertex_separation.py
+++ hamiltonian/vertex_separation.py
@@ -179,6 +179,7 @@ class VertexSeparation:
               **kwds) -> Tuple[int, Dict[Hashable, int]]:
 
         maxsat_solver = RC2Stratified if stratified else RC2
+        print(f"{'' if stratified else 'un'}stratified")
         max_solver = maxsat_solver(self._cnf, solver = solver, **kwds)
         soln = max_solver.compute()
         if kwds.get('verbose', 0) > 0:
