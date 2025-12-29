4a6fe9f31a1a4c774590fa019d6c16409aa3b74a
Victor S. Miller <victorsmiller@gmail.com>
2025-12-28 16:07:14 -0800

    Simplified by adding all edges, and then restricting to the vertices

--- hamiltonian/grid.py
+++ hamiltonian/grid.py
@@ -5,14 +5,13 @@ from itertools import product
 import networkx as nx
 from .util import GraphOrder, get_count
 
-def grid_graph(num: int, mnum: int = 0) -> nx.Graph:
-    mnum = num if mnum == 0 else mnum
-    board = set(product(range(num), range(mnum)))
+def grid_graph(num: int, mnum: int | None = None) -> nx.Graph:
+    pnum = num if mnum is None 0 else mnum
+    board = set(product(range(num), range(pnum)))
     nbrs = {(1,0), (-1,0), (0,1), (0,-1)}
     gph = nx.Graph()
     for square in board:
         for delta in nbrs:
-            nbr = (square[0] + delta[0], square[1] + delta[1])
-            if nbr in board:
-                gph.add_edge(square, nbr)
+            gph.add_edge(square, (square[0] + delta[0], square[1] + delta[1]))
+    gph.remove_nodes_from((_ for _ in gph if _ not in board))
     return gph
--- hamiltonian/knights.py
+++ hamiltonian/knights.py
@@ -5,15 +5,15 @@ from itertools import product
 import networkx as nx
 from .util import GraphOrder, get_count
 
-def knight_graph(num: int) -> nx.Graph:
+def knight_graph(num: int, mnum: int | None = None) -> nx.Graph:
 
+    pnum = num if mnum is None else mnum
     nbrs = {(1,2), (1,-2), (-1, 2), (-1, -2),
             (2, 1), (2,-1), (-2, 1), (-2, -1)}
-    board = set(product(range(num), repeat = 2))
+    board = set(product(range(num), range(pnum)))
     gph = nx.Graph()
     for square in board:
         for delta in nbrs:
-            nbr = (square[0] + delta[0], square[1] + delta[1])
-            if nbr in board:
-                gph.add_edge(square, nbr)
+            gph.add_edge(square, (square[0] + delta[0], square[1] + delta[1]))
+    gph.remove_nodes_from((_ for _ in gph if _ not in board))
     return gph
