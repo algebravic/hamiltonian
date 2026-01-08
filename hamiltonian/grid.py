8677d48cba17b3e350690a4f4db8c2847bbec257
Victor S. Miller <victorsmiller@gmail.com>
2026-01-08 10:50:52 -0800

    Added a graph name

--- hamiltonian/grid.py
+++ hamiltonian/grid.py
@@ -9,9 +9,9 @@ def grid_graph(num: int, mnum: int | None = None) -> nx.Graph:
     pnum = num if mnum is None else mnum
     board = set(product(range(num), range(pnum)))
     nbrs = {(1,0), (-1,0), (0,1), (0,-1)}
-    gph = nx.Graph(f'Grid({num},{pnum})')
+    gph = nx.Graph(name=f'Grid({num},{pnum})')
     for square in board:
         for delta in nbrs:
             gph.add_edge(square, (square[0] + delta[0], square[1] + delta[1]))
-    gph.remove_nodes_from((_ for _ in gph if _ not in board))
+    gph.remove_nodes_from([_ for _ in gph if _ not in board])
     return gph
